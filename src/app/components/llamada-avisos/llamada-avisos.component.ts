24476af4af3e9ab3bf551d5d05e2783f712283d4
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-09-07 17:47:01 +0200

    challenge advice completo

--- src/app/components/llamada-avisos/llamada-avisos.component.ts
+++ src/app/components/llamada-avisos/llamada-avisos.component.ts
@@ -7,9 +7,28 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./llamada-avisos.component.css']
 })
 export class LlamadaAvisosComponent implements OnInit {
-  cssUrl:string;
+  public cssUrl:string;
+  public titleAdvice:string;
+  public advice:string;
+
   constructor(public sanitizer: DomSanitizer) {
     this.cssUrl='/assets/css/styles.css';
+    this.titleAdvice="";
+    this.advice=""
+  }
+
+  generarAvisos(){
+    fetch('https://api.adviceslip.com/advice')
+    .then(response=>response.json())
+    .then(data=>{
+      //Introducimos los datos de la api en las variables titleAdvice y en advice
+      this.titleAdvice="ADVICE #"+data.slip.id
+      this.advice='"'+data.slip.advice+'"'
+    }
+    
+    )
+    console.log(this.titleAdvice)
+    console.log(this.advice)
   }
   
   ngOnInit(): void {
